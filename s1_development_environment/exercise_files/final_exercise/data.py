170b2330750c7ec8154de4a5da1f9e6898a60b04
Tinghui Cheng <tinghui@MacBook-Pro-2.localdomain>
2024-01-02 21:33:20 +0100

    finished final exercise

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,9 +1,96 @@
 import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import glob
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class TrainDataset(Dataset):
+    def __init__(self):
+        self.imgs_path = "/Users/tinghui/workspace/python/dtu_mlops/data/corruptmnist/"
+        
+        image = glob.glob(self.imgs_path + "*train_image*")
+        # label = glob.glob(self.imgs_path + "*train_target*")
+        self.data = []
+        self.labels = []
+        for image_path in image:
+            
+            root,tail = os.path.split(image_path)
+            label_path = os.path.join(root, tail.split("_")[0]+"_target_"+tail.split("_")[-1])
+
+            loaded_data = torch.load(image_path)
+            loaded_labels = torch.load(label_path)
+            
+            assert len(loaded_data) == len(loaded_labels)
+            
+            self.data.extend(loaded_data)
+            self.labels.extend(loaded_labels)
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            # Add other transformations as needed
+        ])
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        sample = {
+            'data': self.data[idx],
+            'label': self.labels[idx]
+        }
+
+        # Apply transformations if needed
+        if self.transform:
+            if not isinstance(sample['data'], torch.Tensor):
+                sample['data'] = self.transform(sample['data'])
+
+        return sample
+
+class TestDataset(Dataset):
+    def __init__(self):
+        self.imgs_path = "/Users/tinghui/workspace/python/dtu_mlops/data/corruptmnist/"
+        
+        image_path = os.path.join(self.imgs_path, "test_images.pt")
+        label_path = os.path.join(self.imgs_path, "test_target.pt")
+        
+        self.data =  torch.load(image_path)
+        self.labels =  torch.load(label_path)
+
+        assert len(self.data) == len(self.labels)
+
+        # You may need to preprocess the data or apply transformations here
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            # Add other transformations as needed
+        ])
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        sample = {
+            'data': self.data[idx],
+            'label': self.labels[idx]
+        }
+
+        # Apply transformations if needed
+        if self.transform:
+            if not isinstance(sample['data'], torch.Tensor):
+                sample['data'] = self.transform(sample['data'])
+
+        return sample
 
 
 def mnist():
     """Return train and test dataloaders for MNIST."""
     # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784)
+    trainset = TrainDataset()
+    testset= TestDataset()
+    batch_size = 64  # Adjust this based on your requirements
+    shuffle = True    # You may want to shuffle the data during training
+    train = DataLoader(trainset, batch_size=batch_size, shuffle=shuffle)
+    test = DataLoader(testset, batch_size=batch_size, shuffle=False)
     return train, test
--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -1,6 +1,8 @@
 import click
 import torch
 from model import MyAwesomeModel
+from torch import nn
+from torch import optim
 
 from data import mnist
 
@@ -10,6 +12,34 @@ def cli():
     """Command line interface."""
     pass
 
+def validation(model, testloader, criterion):
+    """Validate the model on the testdata by calculating the sum of mean loss and mean accuracy for each test batch.
+
+    Arguments:
+        model: torch network
+        testloader: torch.utils.data.DataLoader, dataloader of test set
+        criterion: loss function
+    """
+    accuracy = 0
+    test_loss = 0
+    for batch in testloader:
+
+        # load data and labels in the batch
+        data = batch['data']
+        labels = batch['label']
+
+        output = model.forward(data)
+        test_loss += criterion(output, labels).item()
+
+        ## Calculating the accuracy
+        # Model's output is log-softmax, take exponential to get the probabilities
+        ps = torch.exp(output)
+        # Class with highest probability is our predicted class, compare with true label
+        equality = labels.data == ps.max(1)[1]
+        # Accuracy is number of correct predictions divided by all predictions, just take the mean
+        accuracy += equality.type_as(torch.FloatTensor()).mean()
+
+    return test_loss, accuracy
 
 @click.command()
 @click.option("--lr", default=1e-3, help="learning rate to use for training")
@@ -20,8 +50,54 @@ def train(lr):
 
     # TODO: Implement training loop here
     model = MyAwesomeModel()
-    train_set, _ = mnist()
 
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    epochs = 30
+    steps = 0
+    running_loss = 0
+
+    train_set, test_set = mnist()
+
+
+    train_losses, test_losses = [], []
+
+    for e in range(epochs):
+        for batch in train_set:
+            steps += 1
+            # load data and labels in the batch
+            data = batch['data']
+            labels = batch['label']
+
+            # Training
+            optimizer.zero_grad()
+            log_ps = model(data)
+            loss = criterion(log_ps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            
+        else:
+            model.eval()
+
+            # Turn off gradients for validation, will speed up inference
+            with torch.no_grad():
+                test_loss, accuracy = validation(model, test_set, criterion)
+
+            train_loss = running_loss/len(train_set)
+            train_losses.append(train_loss)
+            test_loss = test_loss / len(test_set)
+            test_losses.append(test_loss)
+            print(
+                "Epoch: {}/{} ".format(e + 1, epochs),
+                "Training Loss: {:.3f} ".format(train_loss),
+                "Test Loss: {:.3f} ".format(test_loss),
+                "Test Accuracy: {:.3f}".format(accuracy / len(test_set)),
+                )
+            running_loss = 0
+            model.train()
+    torch.save(model.state_dict(), 'trained_model.pt')
 
 @click.command()
 @click.argument("model_checkpoint")
@@ -31,8 +107,14 @@ def evaluate(model_checkpoint):
     print(model_checkpoint)
 
     # TODO: Implement evaluation logic here
-    model = torch.load(model_checkpoint)
+    model = MyAwesomeModel()
+    state_dict = torch.load(model_checkpoint)
+    model.load_state_dict(state_dict)  
+     
     _, test_set = mnist()
+    criterion = nn.CrossEntropyLoss()
+    _, accuracy = validation(model, test_set,criterion)
+    print("Accuracy: {:.3f}".format(accuracy / len(test_set)))
 
 
 cli.add_command(train)

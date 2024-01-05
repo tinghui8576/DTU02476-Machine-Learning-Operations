7bcda4f98e88749fb922b1ac3074b34795897776
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-01-05 11:48:14 +0100

    Application logging

---
+++ s4_debugging_and_logging/exercise_files/mylogger.py
@@ -0,0 +1,60 @@
+import logging
+import logging.config
+import sys
+from pathlib import Path
+from rich.logging import RichHandler
+
+LOGS_DIR = "log"
+
+logging_config = {
+    "version": 1,
+    "formatters": { # 
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "root": {
+        "handlers": ["console", "info", "error"],
+        "level": logging.INFO,
+        "propagate": True,
+    },
+}
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger()
+logger.handlers[0] = RichHandler(markup=True)  # set rich handler
+# Create super basic logger
+#logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+#logger = logging.getLogger(__name__) # 
+
+if __name__ == "__main__":
+    # Logging levels (from lowest to highest priority)
+    logger.debug("Used for debugging your code.")
+    logger.info("Informative messages from your code.")
+    logger.warning("Everything works but there is something to be aware of.")
+    logger.error("There's been a mistake with the process.")
+    logger.critical("There is something terribly wrong and process may terminate.")

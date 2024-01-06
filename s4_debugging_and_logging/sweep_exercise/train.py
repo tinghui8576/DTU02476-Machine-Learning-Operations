68cf0837518cbf12e09687aa65fb4f4b510e91af
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-01-06 08:58:07 +0100

    Sweep exercise finished

---
+++ s4_debugging_and_logging/sweep_exercise/train.py
@@ -0,0 +1,183 @@
+"""Adapted from https://github.com/Jackson-Kang/Pytorch-VAE-tutorial/blob/master/01_Variational_AutoEncoder.ipynb.
+
+A simple implementation of Gaussian MLP Encoder and Decoder trained on MNIST
+"""
+import wandb
+import random
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+
+cuda = torch.cuda.is_available()
+DEVICE = torch.device("cuda" if cuda else "cpu")
+dataset_path = "datasets"
+train_dataset = MNIST(dataset_path, transform=transforms.ToTensor(), train=True, download=True)
+test_dataset = MNIST(dataset_path, transform=transforms.ToTensor(), train=False, download=True)
+
+class Encoder(nn.Module):
+    """Gaussian MLP Encoder."""
+
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(Encoder, self).__init__()
+
+        self.FC_input = nn.Linear(input_dim, hidden_dim)
+        self.FC_mean = nn.Linear(hidden_dim, latent_dim)
+        self.FC_var = nn.Linear(hidden_dim, latent_dim)
+        self.training = True
+
+    def forward(self, x):
+        """Forward pass."""
+        h_ = torch.relu(self.FC_input(x))
+        mean = self.FC_mean(h_)
+        log_var = self.FC_var(h_)
+
+        std = torch.exp(0.5 * log_var)
+        z = self.reparameterization(mean, std)
+
+        return z, mean, log_var
+
+    def reparameterization(
+        self,
+        mean,
+        std,
+    ):
+        """Reparameterization trick."""
+        epsilon = torch.randn_like(std)
+
+        z = mean + std * epsilon
+
+        return z
+
+
+class Decoder(nn.Module):
+    """Bernoulli MLP Decoder."""
+
+    def __init__(self, latent_dim, hidden_dim, output_dim):
+        super(Decoder, self).__init__()
+        self.FC_hidden = nn.Linear(latent_dim, hidden_dim)
+        self.FC_output = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        """Forward pass."""
+        h = torch.relu(self.FC_hidden(x))
+        x_hat = torch.sigmoid(self.FC_output(h))
+        return x_hat
+
+
+class Model(nn.Module):
+    """VAE Model."""
+
+    def __init__(self, encoder, decoder):
+        super(Model, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, x):
+        """Forward pass."""
+        z, mean, log_var = self.encoder(x)
+        x_hat = self.decoder(z)
+
+        return x_hat, mean, log_var
+
+
+def loss_function(x, x_hat, mean, log_var):
+    """Reconstruction + KL divergence losses summed over all elements and batch."""
+    reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction="sum")
+    kld = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+    return reproduction_loss + kld
+
+
+def train(config=None):
+    # start a new wandb run to track this script
+    wandb.init(config=config, project="sweep_exercise")
+    config = wandb.config
+
+    # Model Hyperparameters
+    batch_size = config.batch_size
+    x_dim = config.x_dim
+    hidden_dim = config.hidden_dim
+    latent_dim = config.latent_dim
+    lr = config.lr
+    epochs = config.epochs
+    print(config)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
+    decoder = Decoder(latent_dim=latent_dim, hidden_dim=hidden_dim, output_dim=x_dim)
+
+    model = Model(encoder=encoder, decoder=decoder).to(DEVICE)
+
+
+    BCE_loss = nn.BCELoss()
+    optimizer = Adam(model.parameters(), lr=lr)
+    print("Start training VAE...")
+    model.train()
+    for epoch in range(epochs):
+        overall_loss = 0
+        for batch_idx, (x, _) in enumerate(train_loader):
+            if batch_idx % 100 == 0:
+                print(batch_idx)
+            x = x.view(batch_size, x_dim)
+            x = x.to(DEVICE)
+
+            optimizer.zero_grad()
+
+            x_hat, mean, log_var = model(x)
+            loss = loss_function(x, x_hat, mean, log_var)
+
+            overall_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+
+            
+        print(
+            "\tEpoch",
+            epoch + 1,
+            "complete!",
+            "\tAverage Loss: ",
+            overall_loss / (batch_idx * batch_size),
+        )
+        wandb.log({"val_loss": overall_loss / (batch_idx * batch_size)})
+    print("Finish!!")
+
+    # Generate reconstructions
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (x, _) in enumerate(test_loader):
+            if batch_idx % 100 == 0:
+                print(batch_idx)
+            x = x.view(batch_size, x_dim)
+            x = x.to(DEVICE)
+            x_hat, _, _ = model(x)
+            break
+
+    save_image(x.view(batch_size, 1, 28, 28), "orig_data.png")
+    save_image(x_hat.view(batch_size, 1, 28, 28), "reconstructions.png")
+
+    # Generate samples
+    with torch.no_grad():
+        noise = torch.randn(batch_size, latent_dim).to(DEVICE)
+        generated_images = decoder(noise)
+
+    save_image(generated_images.view(batch_size, 1, 28, 28), "generated_sample.png")
+    wandb.finish()
+
+if __name__ == "__main__":
+    config_defaults = {
+        "batch_size": 16, 
+        "x_dim" : 784,
+        "hidden_dim" : 400,
+        "latent_dim" : 20,
+        "lr" : 1e-3,
+        "epochs" : 10}
+    train(config_defaults)
+    
+    

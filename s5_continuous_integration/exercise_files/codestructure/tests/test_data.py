bb8f2da058a5db3c5301bb966b7dbd284959c686
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-01-08 13:23:23 +0100

    Add skip command if file is not exist

--- s5_continuous_integration/exercise_files/codestructure/tests/test_data.py
+++ s5_continuous_integration/exercise_files/codestructure/tests/test_data.py
@@ -1,7 +1,9 @@
 import os
 import torch
-
+import pytest
 from tests import _PATH_DATA
+# To skip your data tests if the corresponding data files does not exist
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
 def test_data():
     train = torch.load(os.path.join(_PATH_DATA, "train_image.pt"))
     test = torch.load(os.path.join(_PATH_DATA, "test_image.pt"))

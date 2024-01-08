f7e1fcc93648fc58f57acf83747a6c3a805c13c7
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-01-08 13:17:00 +0100

    [Finished] train unitest

--- s5_continuous_integration/exercise_files/codestructure/tests/test_train.py
+++ s5_continuous_integration/exercise_files/codestructure/tests/test_train.py
@@ -1,15 +1,37 @@
 import torch
 import os
 from src.models.model import MyNeuralNet
-from src.train_model import TrainDataset
-from torch.utils.data import DataLoader
+from torch import nn
+import src.train_model as training
 from tests import _PATH_DATA
-def test_output():
-    trainset = TrainDataset()
-    data = trainset[1]["data"]
-    data= data.view(1,784)
+def test_dataloader():
+    train,test = training.dataloader()
+    assert len(train) > 0 and len(test) >0
+def test_train():
+    # trainset = training.TrainDataset()
+    # data = trainset[1]["data"]
+    # labels = trainset[1]["label"]
+    # data= data.view(1,784)
+    # model = MyNeuralNet()
+    # criterion = nn.CrossEntropyLoss()
+    # output = model(data)
+    # ps = torch.exp(output)
+    # loss = criterion(output , labels)
+
     model = MyNeuralNet()
-    assert data.shape == torch.Size([1,784])
-    output = model(data)
-    ps = torch.exp(output)
-    assert ps.shape == torch.Size([1,10])
+
+    criterion = nn.CrossEntropyLoss()
+
+    train_set, test_set = training.dataloader()
+    
+    for batch in train_set:
+        # load data and labels in the batch
+        data = batch['data']
+        labels = batch['label']
+
+        # Training
+        log_ps = model(data)
+        loss = criterion(log_ps, labels)
+        loss.backward()
+        assert isinstance(loss.item(), float)
+        assert not torch.any(torch.isnan(loss)).item()

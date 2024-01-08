2fb9293395d3609cb0a0f4bd2f9f07f144848e28
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-01-08 13:17:15 +0100

    Adding example for raised warning

--- s5_continuous_integration/exercise_files/codestructure/src/models/model.py
+++ s5_continuous_integration/exercise_files/codestructure/src/models/model.py
@@ -30,6 +30,10 @@ class MyNeuralNet(torch.nn.Module):
             Output tensor with shape [N,out_features]
 
         """
+        # if x.ndim != 4:
+        #     raise ValueError('Expected input to a 4D tensor')
+        # if x.shape[1] != 1 or x.shape[2] != 28 or x.shape[3] != 28:
+        #     raise ValueError('Expected each sample to have shape [1, 28, 28]')
         # make sure input tensor is flattened
         x = x.view(x.shape[0], -1)
         
